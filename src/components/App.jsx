e003f6ca19c4a040d71fdb435c520698ffb24334
Andrii Chychkan <mywork1728@gmail.com>
2023-08-31 16:35:52 +0300

    started ligic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,9 @@
+import Cast from 'pages/Cast';
 import Home from 'pages/Home';
+import Details from 'pages/MovieDetails';
 import Movies from 'pages/Movies';
 import NotFound from 'pages/NotFound';
+import Reviews from 'pages/Reviews';
 import { Routes, Route, Link } from 'react-router-dom';
 
 export const App = () => {
@@ -12,7 +15,12 @@ export const App = () => {
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Movies" element={<Movies />} />
+        <Route path="/Movies" element={<Movies />}>
+          <Route path=":movieId" element={<Details />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+        </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,11 @@ const Home = () => {
   return (
     <>
       <h3>Home</h3>
+      <div>
+        {['film-1', 'film-2', 'film-3', 'film-4', 'film-5'].map(el => {
+          return <div>{el}</div>;
+        })}
+      </div>
     </>
   );
 };

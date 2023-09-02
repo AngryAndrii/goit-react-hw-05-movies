3f224955c7bdad92f16228acb7d0929142cd069b
Andrii Chychkan <mywork1728@gmail>
2023-09-02 23:31:10 +0300

    added reviews query

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,13 +1,37 @@
-import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import { reviewsQuery } from 'services/Api';
 
 const Reviews = () => {
   const { movieId } = useParams();
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    const fetchReviews = async () => {
+      try {
+        const reviews = await reviewsQuery(movieId);
+        setReviews(reviews);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchReviews();
+  }, [movieId]);
 
   return (
-    <>
-      <h4>Reviews</h4>
-      <p>{movieId}</p>
-    </>
+    reviews && (
+      <>
+        <h4>Reviews</h4>
+        <ul>
+          {reviews?.map(({ author, content, id }) => (
+            <li key={id}>
+              <p>author:{author}</p>
+              <p>{content}</p>
+            </li>
+          ))}
+        </ul>
+      </>
+    )
   );
 };
 

4f20905644dbf871d06c358646421a33dfe324a3
Andrii Chychkan <mywork1728@gmail>
2023-09-02 23:10:52 +0300

    added cast query

--- src/components/Cast.jsx
+++ src/components/Cast.jsx
@@ -1,8 +1,48 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import { castQuery } from 'services/Api';
+
 const Cast = () => {
+  const { movieId } = useParams();
+  const [cast, setCast] = useState([]);
+  const defaultImg =
+    'https://ireland.apollo.olxcdn.com/v1/files/0iq0gb9ppip8-UA/image;s=1000x700';
+  useEffect(() => {
+    const fetchCast = async () => {
+      try {
+        const casting = await castQuery(movieId);
+        console.log(casting);
+        setCast(casting);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchCast();
+  }, [movieId]);
+
   return (
-    <>
-      <h4>Cast</h4>
-    </>
+    cast && (
+      <>
+        <h4>Cast</h4>
+        <ul style={{ display: 'flex', flexDirection: 'row', gap: '30px' }}>
+          {cast?.map(({ name, character, profile_path, id }) => (
+            <li key={id}>
+              <img
+                src={
+                  profile_path
+                    ? `https://image.tmdb.org/t/p/w200/${profile_path}`
+                    : defaultImg
+                }
+                alt={name}
+                width={150}
+              />
+              <p>name: {name}</p>
+              <p>character: {character}</p>
+            </li>
+          ))}
+        </ul>
+      </>
+    )
   );
 };
 
--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -12,7 +12,6 @@ const Details = () => {
     const fetchDetails = async () => {
       try {
         const detail = await detailsQuery(movieId);
-        console.log(detail);
         setDetails(detail);
       } catch (error) {
         console.log(error);
@@ -21,38 +20,45 @@ const Details = () => {
     fetchDetails();
   }, [movieId]);
 
-  const { original_title, release_date, vote_average, overview, genres } =
-    details;
+  const {
+    original_title,
+    release_date,
+    vote_average,
+    overview,
+    genres,
+    poster_path,
+  } = details;
+
   return (
-    <>
-      <h3>Details</h3>
+    details && (
+      <>
+        <h3>Details</h3>
+        <img
+          src={`https://image.tmdb.org/t/p/w300/${poster_path}`}
+          alt={original_title}
+        />
 
-      <img
-        src={`https://www.themoviedb.org/t/p/w200${details.poster_path}`}
-        alt=""
-      />
-
-      <h3>
-        {original_title} {release_date}
-      </h3>
-      <p>user score {vote_average}</p>
-      <p>{overview}</p>
-      <ul>
-        {genres?.map(({ id, name }) => (
-          <li key={id}>{name}</li>
-        ))}
-      </ul>
-
-      <ul>
-        <li>
-          <Link to="cast">Cast</Link>
-        </li>
-        <li>
-          <Link to="reviews">Reviws</Link>
-        </li>
-      </ul>
-      <Outlet />
-    </>
+        <h3>
+          {original_title} {release_date}
+        </h3>
+        <p>user score {vote_average}</p>
+        <p>{overview}</p>
+        <ul>
+          {genres?.map(({ id, name }) => (
+            <li key={id}>{name}</li>
+          ))}
+        </ul>
+        <ul>
+          <li>
+            <Link to="cast">Cast</Link>
+          </li>
+          <li>
+            <Link to="reviews">Reviws</Link>
+          </li>
+        </ul>
+        <Outlet />
+      </>
+    )
   );
 };
 
